99077dfa507a280a7891d566f592b32b276eb07d
Stanislav Ivanov <alchster@gmail.com>
2017-11-15 18:32:45 +0300

    added /getdata route. added /stat route without testing

--- server/views.py
+++ server/views.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 from flask import Blueprint, render_template, redirect, request, current_app
 from jinja2 import TemplateNotFound
@@ -8,6 +9,8 @@ from .util.reader import Reader
 from .util.tablename import tablename
 from .models.urls import URLs
 from .models.data import Data
+
+from .fetcher import Fetcher
 
 bp = Blueprint("ad-stats", __name__,
                static_folder="../web/static",
@@ -22,6 +25,13 @@ def index(page):
         return render_template("%s.html" % page)
     except TemplateNotFound:
         return render_template("404.html")
+
+
+@bp.route("/stat/<name>")
+def stat(name):
+    session = current_app.db.session
+    return render_template("stat.html",
+                           data=session.query(Data.model(name)).all())
 
 
 @bp.route("/config", methods=["POST"])
@@ -84,8 +94,33 @@ def update():
     return render_template("success.html")
 
 
-@bp.route("/test")
-def test():
-    from datetime import date
+@bp.route("/getdata")
+def getdata():
+    def get_parser(session, table_name):
+        # TODO: create table if not exists
+        DataTable = Data.model(table_name)
+
+        def parser(row):
+            row[0] = datetime.strptime(row[0], "%Y-%m-%d").date()
+            data = session.query(DataTable).filter_by(date=row[0]).first()
+            row = dict(zip(["date", "shows", "starts", "clicks"], row))
+            if data is None:
+                session.add(DataTable(**row))
+            else:
+                data.shows = row["shows"]
+                data.starts = row["starts"]
+                data.clicks = row["clicks"]
+            session.commit()
+
+        return parser
+
     session = current_app.db.session
+    for url in session.query(URLs).all():
+        for row in Fetcher(url.url,
+                           username=url.username,
+                           password=url.password,
+                           row_handler=get_parser(session, url.table)):
+            pass
+    session.commit()
+
     return render_template("success.html")
