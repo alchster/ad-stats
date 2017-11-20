a7ad16b8ef29fe7d3aeb577c9b1d100c7f8b29ca
Stanislav Ivanov <alchster@gmail.com>
2017-11-21 02:08:24 +0300

    tried to fix tablename function

--- server/util/tablename.py
+++ server/util/tablename.py
@@ -1,5 +1,8 @@
 def tablename(string):
-    tn = "".join(filter(str.isalnum, string.encode("ascii", "ignore")))
+    func = str.isalnum
+    if isinstance(string, unicode):
+        func = unicode.isalnum
+    tn = "".join(filter(func, string.encode("ascii", "ignore")))
     if tn == "":
         tn = "__noname"
     if tn[0].isdigit():
