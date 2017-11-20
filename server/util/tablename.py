dfcef2fce2db43793bd4478e0b72e281ae1515d9
Stanislav Ivanov <alchster@gmail.com>
2017-11-21 02:17:38 +0300

    removed fix. added debug message

--- server/util/tablename.py
+++ server/util/tablename.py
@@ -1,8 +1,8 @@
+import logging
+
 def tablename(string):
-    func = str.isalnum
-    if isinstance(string, unicode):
-        func = unicode.isalnum
-    tn = "".join(filter(func, string.encode("ascii", "ignore")))
+    logging.debug("Transforming '%s' to table name", string)
+    tn = "".join(filter(str.isalnum, string.encode("ascii", "ignore")))
     if tn == "":
         tn = "__noname"
     if tn[0].isdigit():
